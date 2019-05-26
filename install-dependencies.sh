61eaa60a5b4db25c20f35b091b4a47a6e77f0297
Adam Mansfield <me@adammansfield.com>
2019-05-26 13:16:45 -0400

    Modify .travis.yml to use install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -2,9 +2,10 @@
 
 dependencies="texlive texlive-latex-extra"
 
+echo "Installing dependencies: $dependencies"
 if [ $(id -u) = 0 ]; then
   apt update -qq
-	apt install -y $dependencies
+  apt install -y $dependencies
 else
   sudo apt update -qq
   sudo apt install -y $dependencies
